46c4c642c9d4c9d430f7c6422029f7ad043ab899
Wesley Cunha Padilha <wesley.padilha@yaman.com.br>
2022-11-18 09:06:16 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de  gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de testes:
-console.log ("O Paulo fez esse commit sem voce ver na aula")
+console.log("O Paulo fez esse commit sem voce ver na aula")
+console.log ("Enviando para o GitHub")
